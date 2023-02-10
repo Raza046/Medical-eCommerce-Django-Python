ba4475dfb692919dde9f1a7afc4565526c223603
Raza Ul Mustafa <55105463+Raza046@users.noreply.github.com>
2023-02-10 23:01:11 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo " BUILD START"
+python3.9  -m pip install -r requirements.txt
+python3.9 manage.py collectstatic  --noinput --clear
+echo " BUILD END"
