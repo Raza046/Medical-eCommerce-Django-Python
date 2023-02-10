819a82a8174b20e70449f33e01882ba01a767bfa
Raza Ul Mustafa <55105463+Raza046@users.noreply.github.com>
2023-02-10 23:03:41 +0500

    Update settings.py

--- medico/settings.py
+++ medico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$owio9l$!p*_c-(#k5-wqdh5!xy&b_s7nt1bk_g0t@_5agz=x('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -128,11 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 EMAIL_USE_TLS = True,
